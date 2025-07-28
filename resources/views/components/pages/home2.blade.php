1d74a8c8a5b6802270d178ca1b682c85cef36ecf
Jacinta Wambui Waweru <lexc92603@gmail.com>
2025-07-28 11:40:26 +0300

    home2 and home changes

--- resources/views/components/pages/home2.blade.php
+++ resources/views/components/pages/home2.blade.php
@@ -1,1 +1,32 @@
-<?php
+@extends('components.layout.app')
+
+@section('content')
+
+<div class="row">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="pg-title">
+                    <h1>Your Dream Career Awaits</h1>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="page-content">
+                    <p>Discover life-changing job opportunities across top global cities. Apply today and take the first step toward your future.</p>
+                    <form>
+                        <input type="search" name="search" placeholder="Search">
+                    </form>
+                    <div class="dream-img">
+                        <img src="{{ asset('images/v27_51.png')}}">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="apply-section">
+                    <a href="apply.html">Apply now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
