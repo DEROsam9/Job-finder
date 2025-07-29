39399026701eb7dd2b33eeda9d4f06897be06a8f
Jacinta Wambui Waweru <lexc92603@gmail.com>
2025-07-29 09:58:00 +0300

    header updates

--- resources/views/components/includes/header.blade.php
+++ resources/views/components/includes/header.blade.php
@@ -3,41 +3,24 @@
         <div class="container">
             <div class="col-md-12">
                 <div class="logo">
-                    <h2>Logo</h2>
-                </div>
+                    <a href="{{ route('landing') }}#home">  
+                    {{-- <h2>Logo</h2> --}}
+<img src="{{ asset('images/TALENTBRIDGE LOGO.png') }}" alt="TalentBridge Logo" height="50">                
+</div>
                 <input class="menu-btn" type="checkbox" id="menu-btn" />
                 <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
                 <ul class="menu">
-                    <li class="active">
-                        <a href="#home" onclick="scrollToSection(event, 'home')">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about-us" onclick="scrollToSection(event, 'about-us')">About Us</a>
-                    </li>
-                    <li>
-                        <a href="#services" onclick="scrollToSection(event, 'services')">Services</a>
-                    </li>
-                    <li>
-                        <a href="#testimonials" onclick="scrollToSection(event, 'testimonials')">Testimonials</a>
-                    </li>
-                    <li>
-                        <a href="#partners" onclick="scrollToSection(event, 'partners')">Partners</a>
-                    </li>
+                    <li class="active"><a href="{{ route('landing') }}#home">Home</a></li>
+                    <li><a href="{{ route('landing') }}#about-us">About Us</a></li>
+                    <li><a href="{{ route('landing') }}#services">Services</a></li>
+                    <li><a href="{{ route('landing') }}#testimonials">Testimonials</a></li>
+                    <li><a href="{{ route('landing') }}#partners">Partners</a></li>
                     <div class="rightmenu">
                         <li class="apply"><a href="{{ route('application') }}">Apply Now</a></li>
                         <li class="track"><a href="{{ route('track.application') }}">Track Application</a></li>
                     </div>
                 </ul>
 
-                <script>
-                function scrollToSection(event, sectionId) {
-                    event.preventDefault();
-                    const section = document.getElementById(sectionId);
-                    if (section) {
-                        section.scrollIntoView({ behavior: 'smooth' });
-                    }
-                }
-                </script>
 
             </div>
         </div>
@@ -60,8 +43,9 @@
     <div class="row">
         <div class="container">
             <div class="col-md-12">
+            <a href="{{ route('landing') }}#home">
                 <div class="logo">
-                    <h2>Logo</h2>
+                    
                 </div>
                 <input class="menu-btn" type="checkbox" id="menu-btn" />
                 <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
